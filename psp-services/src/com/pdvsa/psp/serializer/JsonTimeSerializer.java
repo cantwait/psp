55a0127ff0499c33fec451f659a401d4fbe026b7
cantwait <cadenas.rafael@gmail.com>
2013-06-22 15:50:09 -0430

    1

---
+++ psp-services/src/com/pdvsa/psp/serializer/JsonTimeSerializer.java
@@ -0,0 +1,27 @@
+package com.pdvsa.psp.serializer;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.codehaus.jackson.JsonGenerator;
+import org.codehaus.jackson.JsonProcessingException;
+import org.codehaus.jackson.map.JsonSerializer;
+import org.codehaus.jackson.map.SerializerProvider;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JsonTimeSerializer extends JsonSerializer<Date> {
+
+	private static final SimpleDateFormat dateFormatter = new SimpleDateFormat("MMM dd HH:mm:ss z yyyy");
+
+	@Override
+	public void serialize(Date date, JsonGenerator gen,
+			SerializerProvider provider) throws IOException,
+			JsonProcessingException {
+		String formattedDate = dateFormatter.format(date);
+		gen.writeString(formattedDate);
+
+	}
+
+}
